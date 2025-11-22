58a3e9a4de3179a91b669d8b8a7655229394d589
matheusnsp <116042913+matheusnsp@users.noreply.github.com>
2025-11-21 23:22:25 -0300

    time transition corrected

--- src/components/Projetos.jsx
+++ src/components/Projetos.jsx
@@ -18,7 +18,7 @@ export default function Projetos({ projetos, language }) {
             className="card"
             initial={{ opacity: 0, scale: 0.8 }}
             whileInView={{ opacity: 1, scale: 1 }}
-            transition={{ duration: 0.4, delay: i * 0.1 }}
+            transition={{ duration: 1, delay: i * 0.1 }}
             viewport={{ once: true }}
             whileHover={{ scale: 1.05 }}
           >
